9549228e7d71e5ba904e2178bab2b09da6c3aa6c
kevin tauzer <kevintauzer97@gmail.com>
2018-10-26 12:35:58 -1000

    My friend said i needed this for my code to work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 console.log("hello world.")
+
+document.getElementById("blaze").addEventListener('click',blazeAlert)
+
+function blazeAlert(){
+    alert("Caution you're about to blaze the 69 outta shit!!!")
+}
